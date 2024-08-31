46fb916c2909298e92e7a1a1a5e1ba2ecdd8fb95
WillamesVital <willamescfvital@gmail.com>
2024-08-31 12:17:43 -0300

    Adicionar arquivo de loginPages

---
+++ tests/pages/loginPages.ts
@@ -0,0 +1,32 @@
+// pages/login.page.ts
+
+import { Page } from '@playwright/test';
+import { LoginLocators } from '../locators/loginLocators.ts';
+
+export class LoginPage {
+  constructor(private page: Page) {}
+
+  async openSite() {
+    await this.page.goto('/');
+  }
+
+  async fillUsername(username: string) {
+    await this.page.locator(LoginLocators.usernameInput).fill(username);
+  }
+
+  async fillPassword(password: string) {
+    await this.page.locator(LoginLocators.passwordInput).fill(password);
+  }
+
+  async submitLogin() {
+    await this.page.locator(LoginLocators.loginButton).click();
+  }
+
+  async getTitle(): Promise<string> {
+    return this.page.title();
+  }
+
+  async getErrorMessage(): Promise<string> {
+    return this.page.locator(LoginLocators.errorMessage).innerText();
+  }
+}
