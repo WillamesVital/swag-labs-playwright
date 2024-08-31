7335870fc994394d8ce7fe507a35c3244cc22f37
WillamesVital <willamescfvital@gmail.com>
2024-08-31 12:17:55 -0300

    Adicionar arquivo de loginLocators

---
+++ tests/locators/loginLocators.ts
@@ -0,0 +1,7 @@
+export const LoginLocators = {
+    usernameInput: '#user-name',
+    passwordInput: '#password',
+    loginButton: 'text=Login',
+    errorMessage: '//div[@class="error-message-container error"]',
+  };
+  
