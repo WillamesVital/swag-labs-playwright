669d4acf43d57b979ca5c1570e9ef471deda6f00
WillamesVital <willamescfvital@gmail.com>
2024-10-28 23:16:31 -0300

    teste variavel

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,19 +6,21 @@ import { defineConfig, devices } from '@playwright/test';
  */
 // import dotenv from 'dotenv';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
+Object.assign(global, {
+  PROD_URL: process.env.PROD || 'https://www.saucedemo.com',
+  QAS_URL: process.env.BASE_URL || 'https://www.saucedemoqa.com',
+});
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+// Object.assign(global, {
+// //   QAS_URL: process.env.BASE_URL || 'https://new-site-ecommerce-qas.ferreiracosta.com',
+// // });
+
+
 export default defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
@@ -26,11 +28,6 @@ export default defineConfig({
   use: {
       // ...
       trace: "on",
-  
-    /* Base URL to use in actions like `await page.goto('/')`. */
-   baseURL: 'https://www.saucedemo.com',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
   },
 
   /* Configure projects for major browsers */
